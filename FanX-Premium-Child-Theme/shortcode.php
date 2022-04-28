3b2fcf223d37875e09d97a00dbd34197b9bb50e6
Elizabeth Moore <liz@dancingfraxinus.com>
2022-04-28 16:04:18 -0600

    Small Tweaks & Shortcode Additions
    Added shortcode for page content & updated Header Styling to break words instead of wrap.

--- FanX-Premium-Child-Theme/shortcode.php
+++ FanX-Premium-Child-Theme/shortcode.php
@@ -1,13 +1,29 @@
 <?php
 
-//Page Title
+// -- [page_title] -->
 
 function page_title_df( ){
    return get_the_title();
 }
 add_shortcode( 'page_title', 'page_title_df' );
 
-/* ------- Line Break Shortcode --------*/
+// -- [page_content] -->
+
+function page_content_df( ){
+   return get_the_content();
+}
+add_shortcode( 'page_content', 'page_content_df' );
+
+// -- [tax_venue] -->
+
+function tax_venue_df($atts){
+   return get_the_terms( $post, 'venue' );
+}
+add_shortcode( 'tax_venue', 'tax_venue_df' );
+
+
+
+// -- [br] -->
 function line_break_df() {
 	return '<br />';
 }
